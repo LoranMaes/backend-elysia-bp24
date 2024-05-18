f1cd6605502bae8ee94697b5850a509981adf7f4
Loran Maes <loran.maes@student.odisee.be>
2024-05-18 21:11:09 +0200

    Added necessary packages (cors, rate limiter, bearer ...) & added guards for protected routes

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,25 @@
 import { Elysia } from "elysia";
 import apiRoutes from "./routes/api";
 import swagger from "@elysiajs/swagger";
+import cors from "@elysiajs/cors";
+import { rateLimit } from "elysia-rate-limit";
 
 const app = new Elysia();
 
+// EXAMPLE: Using cors with regex
+// .use(cors({
+//   origin: /.*\.saltyaom\.com$/
+// }))
+
 app
+  .use(rateLimit())
   .use(swagger())
+  .use(cors())
   .group("api", (app) => app.use(apiRoutes))
+  .onError(({ error, code, set }) => {
+    set.status = 500;
+    return "Internal Server Error";
+  })
   .listen(Bun.env.PORT || 3000);
 
 console.log(
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -4,14 +4,19 @@ import taskRoutes from "./tasks";
 import statsRoutes from "./statistics";
 import catRoutes from "./categories";
 import adminRoutes from "./admin";
+import bearer from "@elysiajs/bearer";
 
 const apiRoutes = new Elysia();
 
 apiRoutes
   .group("/auth", (app) => app.use(authRoutes))
-  .group("/tasks", (app) => app.use(taskRoutes))
-  .group("/statistics", (app) => app.use(statsRoutes))
-  .group("/categories", (app) => app.use(catRoutes))
-  .group("/admin", (app) => app.use(adminRoutes));
+  .use(bearer())
+  .guard({}, (app) =>
+    app
+      .group("/tasks", (app) => app.use(taskRoutes))
+      .group("/statistics", (app) => app.use(statsRoutes))
+      .group("/categories", (app) => app.use(catRoutes))
+      .guard({}, (app) => app.group("/admin", (app) => app.use(adminRoutes)))
+  );
 
 export default apiRoutes;
