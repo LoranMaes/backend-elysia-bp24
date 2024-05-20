5e487c09230aa7a9b482b5fb3a26c52cc0bf3e7c
Loran Maes <loran.maes@student.odisee.be>
2024-05-20 19:32:45 +0200

    Added logout functionality

--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -1,6 +1,7 @@
 import Elysia from "elysia";
 import { generalSchema } from "../routeValidators";
 import { AuthService } from "../../services/auth.service";
+import { lucia } from "../../libs/auth";
 
 const authRoutes = new Elysia({ prefix: "/auth" });
 const Auth = AuthService;
@@ -24,15 +25,24 @@ authRoutes
   )
   .post(
     "/login",
-    async ({ body }) => {
-      const user = await Auth.login(body);
-      return { message: "User logged in", data: body };
+    async ({ body, error }) => {
+      try {
+        return await Auth.login(body);
+      } catch (err) {
+        return error(500, err);
+      }
     },
     {
       body: "route.login",
       type: "multipart/form-data",
     }
   )
-  .post("/logout", () => Auth.logout());
+  .post("/logout", async ({ cookie: { auth_session }, error }) => {
+    try {
+      return await Auth.logout(auth_session);
+    } catch (err) {
+      return error(500, err);
+    }
+  });
 
 export default authRoutes;
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -6,6 +6,8 @@ import { Cookie, generateIdFromEntropySize } from "lucia";
 import { Language } from "../models/language.enum";
 import { sql } from "drizzle-orm";
 import { createId } from "@paralleldrive/cuid2";
+import { sessions } from "../db/schemas/sessions";
+import { ElysiaCookie } from "elysia/dist/cookies";
 
 export namespace AuthService {
   const createUser = async (
@@ -27,13 +29,13 @@ export namespace AuthService {
     };
   };
 
-  const checkEmailExists = async (email: string): Promise<boolean> => {
-    const emailExists = await db
+  const checkEmailExists = (email: string): boolean => {
+    const emailExists = db
       .select()
       .from(users)
       .where(sql`email = ${email.toLowerCase()}`)
       .get();
-    return Boolean(emailExists);
+    return emailExists ? true : false;
   };
 
   const createSessionAndCookie = async (userId: string): Promise<Cookie> => {
@@ -59,7 +61,7 @@ export namespace AuthService {
   };
 
   export const register = async (props: UserCreation) => {
-    if (await checkEmailExists(props.email)) {
+    if (checkEmailExists(props.email)) {
       return new Response("Email already exists", { status: 400 });
     }
 
@@ -87,9 +89,61 @@ export namespace AuthService {
     });
   };
   export const login = async (props: UserLogin) => {
-    return props;
+    const user = db
+      .select()
+      .from(users)
+      .where(sql`email = ${props.email}`)
+      .get();
+
+    // Tip: For security reasons I'm verifying the password before validating the user
+    // This way you can avoid timing attacks (https://en.wikipedia.org/wiki/Timing_attack)
+    let verifiedPassword = false;
+    if (user)
+      verifiedPassword = await Bun.password.verify(
+        props.password,
+        user.password
+      );
+
+    if (!user) {
+      return new Response("Invalid email or password", { status: 404 });
+    }
+    if (!verifiedPassword) {
+      return new Response("Invalid password or password", { status: 400 });
+    }
+
+    const sessionCookie = await createSessionAndCookie(user.id);
+
+    return new Response(null, {
+      status: 200,
+      headers: {
+        "Set-Cookie": sessionCookie.serialize(),
+      },
+    });
   };
-  export const logout = async () => {
-    return "Logged out successfully!";
+  export const logout = async (auth_session: ElysiaCookie) => {
+    if (!auth_session) {
+      return new Response("You are not logged in", { status: 400 });
+    }
+
+    const session = db
+      .select()
+      .from(sessions)
+      .where(sql`id = ${auth_session.value}`)
+      .get();
+
+    if (!session) {
+      return new Response("You are not logged in", { status: 400 });
+    }
+
+    // Remove the session
+    await lucia.invalidateSession(session.id);
+    const sessionCookie = lucia.createBlankSessionCookie();
+
+    return new Response(null, {
+      status: 200,
+      headers: {
+        "Set-Cookie": sessionCookie.serialize(),
+      },
+    });
   };
 }
